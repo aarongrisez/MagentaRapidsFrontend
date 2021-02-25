1c704d35db8516d76af81e22edd7526e1666b6d4
Aaron Grisez <acgrisez@gmail.com>
2021-02-24 18:42:47 -0800

    Redesigning Frontend

---
+++ src/components/layout/Header.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+
+const Header = (props) => {
+
+    const style = {
+      fontFamily: "'Courier New', Courier, monospace"
+    }
+
+    return (
+        <h1 style={style}>magenta rapids</h1>
+    )
+
+
+}
+
+export default Header;
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { SketchContainer } from "../sketch/SketchContainer";
 import { Audio } from "../audio/Audio";
 import { LastPlayedNote } from "../text/LastPlayedNote";
+import BaseLayout from "../layout/BaseLayout";
+import TimeseriesChart from "../timeseries/TimeseriesChart"
 
 export const HomePage = () => {
+  
+  const branding = {
+    name: "magenta rapids"
+  }
+
+  const links = [
+    {
+      title: "about",
+      href: "/about"
+    },
+    {
+      title: "support",
+      href: "/support"
+    },
+    {
+      title: "contact",
+      href: "/contact"
+    }
+  ]
 
   return (
-    <div>
-      <h1>magenta rapids</h1>
-      <SketchContainer />
-      <Audio />
-      <LastPlayedNote />
-    </div> 
+    <Fragment>
+      <BaseLayout 
+        headerProps={{branding: branding, links: links}}
+        chart={<TimeseriesChart/>}
+        audio={<Audio />}
+        latestNote={<LastPlayedNote/>}
+      />
+    </Fragment>
   )
   
 }
